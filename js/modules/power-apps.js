51874094fbb08281eac437da6a56429e01b4f742
uttne <uttne.mkr@gmail.com>
2024-04-07 00:14:11 +0900

    DataSource を取得する部分を追加

--- js/modules/power-apps.js
+++ js/modules/power-apps.js
@@ -1,5 +1,48 @@
 import "../@types/jszip.t.js";
 
+/**
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/AppCheckerResult.sarif
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/checksum.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/Controls/1.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/Controls/4.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/Header.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/Properties.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/References/DataSources.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/References/ModernThemes.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/References/Resources.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/References/Templates.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/References/Themes.json
+ * N51005195-dd8a-4c56-aedc-4f068951aadd-document/Resources/PublishInfo.json
+ */
+
+/**
+ * @typedef {Object} ManifestDetails
+ * @property {string} displayName
+ * @property {string} description
+ * @property {string} createdTime
+ * @property {string} packageTelemetryId
+ * @property {string} creator
+ * @property {string} sourceEnvironment
+ */
+
+/**
+ * @typedef {Object} Manifest
+ * @property {string} schema
+ * @property {ManifestDetails} details
+ */
+
+/**
+ * @typedef {Object} DataSource
+ * @property {string} ApiId
+ * @property {string} Name
+ * @property {string} Type
+ */
+
+/**
+ * @typedef {Object} DataSourcesJson
+ * @property {DataSource[]} DataSources
+ */
+
 export class PowerApps {
   constructor() {
     /** @type {JSZip|null} */
@@ -8,6 +51,8 @@ export class PowerApps {
     this._msapp = null;
     /** @type {File} */
     this._file = null;
+    /** @type {Manifest} */
+    this.manifest = null;
   }
 
   /**
@@ -26,7 +71,20 @@ export class PowerApps {
 
     const keys = Object.keys(zip.files);
 
-    // TODO manifest ファイルの中身を確認して PowerApps の zip かを判定する
+    const manifestKey = keys.filter((key) => key.endsWith("/manifest.json"))[0];
+    if (!manifestKey) {
+      throw new Error(
+        "This is not PowerApps export file. : manifest.json is not found."
+      );
+    }
+
+    const manifestJson = await zip.files[manifestKey].async("string");
+    /** @type {Manifest} */
+    const manifest = JSON.parse(manifestJson);
+
+    if (!["1.0"].includes(manifest.schema)) {
+      throw new Error(`Schema '${manifest.schema}' is not support.`);
+    }
 
     const msappKey = keys.filter((key) => key.endsWith(".msapp"))[0];
 
@@ -44,5 +102,30 @@ export class PowerApps {
     this._file = file;
     this._zip = zip;
     this._msapp = msapp;
+    this.manifest = manifest;
+  }
+
+  /**
+   *
+   * @returns {Promise<DataSource[]>}
+   */
+  async getDataSourcesAsync() {
+    const msapp = this._msapp;
+    if (!msapp) throw Error("This is not loaded.");
+
+    const keys = Object.keys(msapp.files);
+    const dataSourcesKey = keys.filter((key) =>
+      key.endsWith("/References/DataSources.json")
+    )[0];
+
+    if (!dataSourcesKey) {
+      throw Error("'DataSourses.json' is not found.");
+    }
+
+    const dataSourcesJson = await msapp.files[dataSourcesKey].async("string");
+    /** @type {DataSourcesJson} */
+    const dataSources = JSON.parse(dataSourcesJson);
+
+    return dataSources.DataSources;
   }
 }
