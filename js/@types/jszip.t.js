d0d992dc3bd6cf9d115d3e77438098b9ce31f613
uttne <uttne.mkr@gmail.com>
2024-04-16 03:20:19 +0900

    JSZip の型定義を更新

--- js/@types/jszip.t.js
+++ js/@types/jszip.t.js
@@ -2,24 +2,30 @@
  * @typedef {Object} JSZip
  * @property {function(File|Blob): Promise<JSZip>} loadAsync - 読み込み
  * @property {Object.<string, ZipObject>} files
- * @property {function(GenerateAsyncOptions): Promise<Blob>} generateAsync
+ * @property {function(GenerateAsyncOptions): Promise<Blob|string>} generateAsync
+ * @property {function(string,string,FileOptions)} file
  */
 
 /**
- * @typedef {Object} compressionOptions
+ * @typedef {Object} FileOptions
+ * @property {boolean} base64
+ */
+
+/**
+ * @typedef {Object} CompressionOptions
  * @property {number} level - 9 が最高品質
  */
 
 /**
  * @typedef {Object} GenerateAsyncOptions - https://stuk.github.io/jszip/documentation/api_jszip/generate_async.html
- * @property {string} type - blob : blob 以外を指定することも可能だが考慮が大変なので blob に決め打つ
+ * @property {string} type - blob | base64 : blob, base64 以外を指定することも可能だが考慮が大変なので blob に決め打つ
  * @property {string} compression - DEFLATE
- * @property {compressionOptions} compressionOptions
+ * @property {CompressionOptions} compressionOptions
  */
 
 /**
  * @typedef {Object} ZipObject
  * @property {boolean} dir - dir か否か
  * @property {string} name - パス
- * @property {function(string): Promise<string>} async - 内容の読み込み async("string").then(content=>console.log(content)) のように記述する : "string" | "blob"
+ * @property {function(string): Promise<string|Blob>} async - 内容の読み込み async("string").then(content=>console.log(content)) のように記述する : "string" | "blob"
  */
